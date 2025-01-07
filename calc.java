7899b098e24cfaf24925adb8e4a7768002ae7549
durgesh656 <durgeshkushwaha656@gmail.com>
2025-01-07 16:09:49 +0530

    Division added

--- calc.java
+++ calc.java
@@ -8,6 +8,9 @@ return a-b;
 public static int multiplication(int a , int b){
 return a*b;
 }
+public static int division(int a , int b){
+return a/b
+}
 public static void main (String[] args){
 System.out.println("Durgesh");
 }

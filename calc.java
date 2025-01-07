5b457f57326abe259709b1aafc28b7f372b4bcc6
durgesh656 <durgeshkushwaha656@gmail.com>
2025-01-07 15:58:14 +0530

    multiplication added

--- calc.java
+++ calc.java
@@ -5,6 +5,9 @@ return a+b;
 public static int substract (int a , int b ){
 return a-b; 
 }
+public static int multiplication(int a , int b){
+return a*b;
+}
 public static void main (String[] args){
 System.out.println("Durgesh");
 }

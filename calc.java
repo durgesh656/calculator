6985f0ff9d7b88059617c217e81463f69fdadb7b
durgesh656 <durgeshkushwaha656@gmail.com>
2025-01-07 15:49:34 +0530

    substraction added

--- calc.java
+++ calc.java
@@ -2,6 +2,9 @@ Class calc {
 public static int add (int a , int b ){
 return a+b; 
 }
+public static int substract (int a , int b ){
+return a-b; 
+}
 public static void main (String[] args){
 System.out.println("Durgesh");
 }
